f5e5bc10cfd7da96a46f6c67550615f45f1b68ef
Gerson Borges <passupreto@gmail.com>
2020-01-13 17:18:16 -0300

    Create TiposDeDados.java

---
+++ exemplos/TiposDeDados.java
@@ -0,0 +1,31 @@
+class TiposDeDados {
+
+    // Booleanos
+    boolean verdade = true;
+    boolean mentira = false;
+    boolean usuarioAtivo = false;
+
+    // Int
+    int idade = 18;
+
+    // Um long precisa de um L no final do número
+    // do contrário o compilar entenderá
+    // o número como um int
+    long distancia = 5000L;
+
+    // Double
+    double distanciaDouble = 1.5;
+
+    // Float
+    // Um float precisa de um F no final do número
+    // do contrário o compilador entenderá o
+    // número como um double
+    float distanciaFloat = 1.5F;
+
+    // Char com aspas
+    char cAspas = 'c';
+
+    // Char com número
+    // Char é um tipo numéro sem sinal
+    char cNumero = 99; // 'c'
+}
